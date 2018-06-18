978af0466ecda696755ccc8124c6359b03f0a4f9
Federico Torrisi <fedro.torrisi@gmail.com>
2018-06-18 21:03:24 +0200

    aggiunto config

--- config/tenancy.php
+++ config/tenancy.php
@@ -1,3 +1,24 @@
 <?php
 
-// TODO:
+return [
+
+    'package-store' => \AmcLab\KeymasterStore\Contracts\Messenger::class,
+
+    'resolver' => [
+        'hooks' => [
+            \AmcLab\Tenancy\Hooks\DatabaseHook::class,
+            \AmcLab\Tenancy\Hooks\EncryptionHook::class,
+            \AmcLab\Tenancy\Hooks\MaskingHook::class,
+            //...altri?
+        ],
+    ],
+
+    'api' => [
+        'database-manager' => [
+            'uri' => '/api/amcManager',
+            'key' => '123',
+            'secret' => 'qwerty',
+        ]
+    ]
+
+];
